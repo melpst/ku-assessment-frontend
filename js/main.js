0374170105d954027cc7c11aabd9c7e18d32b1a7
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-05-21 21:54:50 +0700

    add functions and variables

--- js/main.js
+++ js/main.js
@@ -8,18 +8,42 @@ var app = new Vue({
     infoHeaderLabel: 'รายละเอียดคำถาม',
     levelHeaderLabel: 'ระดับความเหมาะสม',
     levelsLabel: ['มากที่สุด', 'มาก', 'พอใช้', 'น้อย', 'น้อยที่สุด'],
-    infosLabel: ['ลักษณะของอาจารย์ เช่น การใช้คำพูด บุคลิกท่าทาง ความรับผิดชอบในการสอน การเป็นแบบอย่างที่ดรแก่นิสิต', 'อาจารย์ฝึกให้นิสิตได้คิดวิเคราะห์ คิดแก้ปัญหา คิดสร้างสรรค์', 'อาจารย์จัดกิจกรรมการเรียนการสอนที่นิสิตได้มีโอกาสแลปเปลี่ยนเรียนรู้กับผู้อื่น', 'อาจารย์จัดกิจกรรมการเรียนการสอนที่มีการส่งเสริมให้นิสิตได้เรียนรู้ด้วยตนเองโดยใช้ทั้งแหล่งการเรียนรู้ หรือเทคโนโลยีสารสนเทศต่างๆ'],
+    infosLabel: ['ลักษณะของอาจารย์ เช่น การใช้คำพูด บุคลิกท่าทาง ความรับผิดชอบในการสอน การเป็นแบบอย่างที่ดีแก่นิสิต', 'อาจารย์ฝึกให้นิสิตได้คิดวิเคราะห์ คิดแก้ปัญหา คิดสร้างสรรค์', 'อาจารย์จัดกิจกรรมการเรียนการสอนที่นิสิตได้มีโอกาสแลกเปลี่ยนเรียนรู้กับผู้อื่น', 'อาจารย์จัดกิจกรรมการเรียนการสอนที่มีการส่งเสริมให้นิสิตได้เรียนรู้ด้วยตนเองโดยใช้ทั้งแหล่งการเรียนรู้ หรือเทคโนโลยีสารสนเทศต่างๆ'],
     submitLabel: 'Submit',
     cancelLabel: 'Cancel',
     logoutLabel: 'Logout',
     subjectId: '01204352',
     sectionId: '1',
     subjectName: 'Laws & Ethics in Information Technology',
-    radioButtonsName: ['great','good','fair','bad','worst']
+    radioButtonsName: ['great','good','fair','bad','worst'],
+    radioDatas: [],
+    keygenIP : 'http://158.108.34.51'
   },
   methods:{
     submitAssess: function(argument) {
       // body...
+    },
+    cancel: function() {
+      this.radioDatas = []
+    },
+    backToKeygen: function() {
+      location.href = keygenIP
     }
+  },
+  created: function() {
+    this.subjectId = getParameterByName('subjectId')
+    this.sectionId = getParameterByName('secId')
+    this.subjectName = decodeURIComponent(getParameterByName('name'))
   }
 })
+
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
